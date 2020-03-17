3d014e38f626cd43eb519e1fda1632a3365bde29
Segev <segev.gersh@gmail.com>
2020-03-17 16:59:28 +0200

    My first bare code for 'arm926ej-s' mcpio (arm-none-eabi-... -mcpu=arm926ej-s) and versatilepb arm system (qemu-system-arm -M versatilepb)

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 # Install qemu
 apt install qemu
 
-# Install arm compiler
+# Install arm compiler (used to create user space out files)
 apt install gcc-arm-linux-gnueabi
+#                      (used to create kernel binaries)
+apt install gcc-arm-none-eabi
--- software/test.c
+++ software/test.c
@@ -1,5 +1,18 @@
-#include <stdio.h>
 
-int main() {
-    printf("hello world\n");
+// Where the UART0 Serial sits
+volatile unsigned char * const UART0_PTR = (unsigned char *)0x0101f1000;
+
+// copy a string to the uart device
+void display(const char *string)
+{
+    while(*string != '\0')
+    {
+        *UART0_PTR = *string;
+        string++;
+    }
 }
+ 
+int my_init(){
+        display("Hello World\n");
+}
+
